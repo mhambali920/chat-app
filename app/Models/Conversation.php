f57467430610bf4c062955643b2cb51787b73d21
mhambali <mhambali920@gmail.com>
2025-04-05 11:34:56 +0700

    add messages relation in conversation

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -15,4 +15,9 @@ class Conversation extends Model
     {
         return $this->belongsToMany(User::class);
     }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
